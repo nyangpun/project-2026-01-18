2845d26bb925ddb0376cb11ddae0f955a0467ef2
nekopunch <taiikichan@naver.com>
2026-01-29 22:39:37 +0900

    review write,detail,list

---
+++ src/main/java/com/backend/domain/review/from/ReviewForm.java
@@ -0,0 +1,11 @@
+package com.backend.domain.review.from;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviewFrom {
+    private String content;
+    private int score;
+}
--- src/main/java/com/backend/domain/review/from/ReviewFrom.java
+++
@@ -1,4 +0,0 @@
-package com.backend.domain.review.from;
-
-public class ReviewFrom {
-}
